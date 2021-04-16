8cb656c52cd5ff3c0fcc2a6869530c39aa25d6b7
shaileshsharma154 <67504757+shaileshsharma154@users.noreply.github.com>
2021-04-16 10:06:31 +0530

    my first commit

---
+++ coronaAPILive.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<center><h2 style="background-color:green;"> Live Corona Case Report <?=date("d-m-Y");?></h2></center>
+<hr >
+
+<table>
+  <tr>
+    <th>Case</th>
+    <th>Count</th>    
+  </tr>
+  <tbody id="global-data">
+  </tbody>
+</table>
+
+<table>
+  <tr style="background-color:pink;">
+    <th>Sr No</th>
+	<th>country name</th>
+    <th>NewConfirmed</th> 
+	<th>NewDeaths</th> 
+	<th>NewRecovered</th> 
+	<th>TotalDeaths</th> 	
+	<th>TotalRecovered</th> 	
+  </tr>
+  <tbody id="countryWise-data">
+  </tbody>
+</table>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+	$.ajax({
+		url:'https://api.covid19api.com/summary',
+		get:'GET',
+		dataType:'json',
+		success:function(data){
+			//console.log(data);
+			//console.log(data.Countries);
+			$.each(data.Global,function(key, value){
+				$.each(function(key, value){
+					$("#dataId").append()
+				});
+				
+				$("#global-data").append("<tr><td>"+ key+"</td><td>"+value+"</td></tr> ");
+			});
+			var x=0;
+			$.each(data.Countries,function(key, value){
+			x++;	
+			$("#countryWise-data").append("<tr><td>"+ x +"</td>"+
+				"<td>"+ value.Country +
+				"</td> <td>"+ value.NewConfirmed+
+				"</td> <td>"+ value.NewDeaths+
+				"</td> <td>"+ value.NewRecovered+
+				"</td> <td>"+ value.TotalDeaths+
+				"</td> <td>"+ value.TotalRecovered+
+				"</td></tr>");
+			 
+			});
+			
+		
+	}
+});
+	
+});
+</script>
+
+</body>
+</html>
